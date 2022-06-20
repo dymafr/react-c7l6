7fde4f630a035b419f04a3527b8605c615b439e3
dymafr <erwan1309@hotmail.com>
2022-06-20 19:03:43 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { useState } from 'react';
 
 function App() {
-  console.log('Rendu du composant App');
-  const [count, setCount] = useState(0);
-  const [showCount, setShowCount] = useState(true);
+  const [user, setUser] = useState({
+    name: '',
+    age: null,
+  });
 
-  function handleClick() {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setShowCount(!showCount);
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(user);
   }
+
+  function handleNameInput(e) {
+    setUser()
+  }
+
+  function handleAgeInput(e) {}
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
-      <button onClick={handleClick} className="mb-20">
-        +1 {showCount && <span> - Valeur : {count}</span>}
-      </button>
+      <form className="d-flex flex-column card p-20">
+        <input
+          onInput={handleNameInput}
+          type="text"
+          className="mb-20"
+          placeholder="Nom"
+        />
+        <input
+          onInput={handleAgeInput}
+          type="number"
+          className="mb-20"
+          placeholder="Age"
+        />
+        <button onClick={handleClick} className="btn btn-primary">
+          Envoi
+        </button>
+      </form>
     </div>
   );
 }
